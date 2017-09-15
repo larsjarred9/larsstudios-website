d7870291a4a3fc92e6d989bc5c37bafa247c7fa1
larsjarred9 <31996561+larsjarred9@users.noreply.github.com>
2017-09-15 21:31:46 +0200

    Update header.cc

--- header.cc
+++ header.cc
@@ -1,3 +1,21 @@
 header
-	height: 450px
-	background: #CCC
+  height: 450px
+  background: #CCC
+
+	.logo
+		+size(50px)
+  		 background: #999
+
+nav
+	A
+		display: inline-block
+		margin-lef: 20px
+		
+		color: rgba(0,0,0,0.7)
+		font
+			weigt: 700
+			size: 14px
+
+		text-transform: uppercase
+		
+	   
